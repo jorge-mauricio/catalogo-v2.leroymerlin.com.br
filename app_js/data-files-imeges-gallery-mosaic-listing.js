b7b67226d9143f80810cc9de4f149541328b3e33
jorge-mauricio <web@jorgemauricio.com>
2021-10-30 16:29:05 -0300

    Gallery mosaic - customization.

---
+++ app_js/data-files-imeges-gallery-mosaic-listing.js
@@ -0,0 +1,24 @@
+const dataJSObjetcFilesImegesGalleryMosaicListing = {
+  files: [
+    {
+      id: 1,
+      id_parent: 0,
+      sort_order: 0,
+      file_type: 1, // 1 - image | 2 - video
+      file_config: 0,
+      title: '',
+      caption: '', // Product id
+      description: '',
+      number_small1: 1, // Image type:  1 - background default | 2 - backgroung feature | 3 - environment photo
+      number_small2: 0, // Image position: 0 - center | 1 - top/left | 2 - top/center | 3 - top/right | 4 - middle/right | 5 - bottom/right | 6 - bottom/center | 7 - bottom/left | 8 - middle/left
+      number_small3: 1, // Text position: 0 - center | 1 - top/left | 2 - top/center | 3 - top/right | 4 - middle/right | 5 - bottom/right | 6 - bottom/center | 7 - bottom/left | 8 - middle/left
+      info_small1: 'http://www.google.com', // External URL
+      file: '',
+      activation: '',
+      ids_link: ['caption123', 'caption456', 'caption789'],
+    },
+  ]
+};
+
+// Debug.
+console.log('dataJSObjetcFilesImegesGalleryMosaicListing=', dataJSObjetcFilesImegesGalleryMosaicListing);
