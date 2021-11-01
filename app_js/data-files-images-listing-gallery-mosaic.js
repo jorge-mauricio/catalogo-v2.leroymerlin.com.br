35364f1e691e478bb4ee0285afe141a52dbc071b
jorge-mauricio <web@jorgemauricio.com>
2021-11-01 05:08:30 -0300

    Layout adjustments.

--- app_js/data-files-images-listing-gallery-mosaic.js
+++ app_js/data-files-images-listing-gallery-mosaic.js
@@ -14,12 +14,13 @@ const dataJSObjetcFilesImegesGalleryMosaicListing = {
       number_small2: 0, // Image position: 0 - center | 1 - top/left | 2 - top/center | 3 - top/right | 4 - middle/right | 5 - bottom/right | 6 - bottom/center | 7 - bottom/left | 8 - middle/left
       number_small3: 1, // Text position: 0 - center | 1 - top/left | 2 - top/center | 3 - top/right | 4 - middle/right | 5 - bottom/right | 6 - bottom/center | 7 - bottom/left | 8 - middle/left
       info_small1: 'http://www.google.com', // External URL
-      file_thumbnail: 'https://picsum.photos/600/600/?image=512',
+      file_thumbnail: 'https://picsum.photos/150/150/?image=512',
       file: 'https://picsum.photos/1920/1080/?image=512',
-      activation: '',
-      ids_link: [],
-      grid_column: 'span 1',
-      grid_row: 'span 3',
+      activation: 1,
+      ids_files: [], // ids (caption) from related files
+      ids_filters: [], // Filters ids
+      grid_column: '1',
+      grid_row: '3',
     },
     {
       id: 2,
@@ -30,16 +31,17 @@ const dataJSObjetcFilesImegesGalleryMosaicListing = {
       title: 'Título do produto de exemplo 02',
       caption: '70843886',
       description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.',
-      number_small1: 1, 
-      number_small2: 0, 
+      number_small1: 2, 
+      number_small2: 4, 
       number_small3: 1, 
       info_small1: 'http://www.google.com',
       file_thumbnail: 'https://picsum.photos/100/100/?image=513',
       file: 'https://picsum.photos/1920/1080/?image=513',
-      activation: '',
-      ids_link: [],
-      grid_column: 'span 3',
-      grid_row: 'span 3',
+      activation: 1,
+      ids_files: [],
+      ids_filters: [],
+      grid_column: '2',
+      grid_row: '3',
     },
     {
       id: 3,
@@ -54,12 +56,13 @@ const dataJSObjetcFilesImegesGalleryMosaicListing = {
       number_small2: 0, 
       number_small3: 1, 
       info_small1: 'http://www.google.com',
-      file_thumbnail: 'https://picsum.photos/600/600/?image=514',
+      file_thumbnail: 'https://picsum.photos/200/200/?image=514',
       file: 'https://picsum.photos/1920/1080/?image=514',
-      activation: '',
-      ids_link: ['90843886', '70843886'],
-      grid_column: 'span 2',
-      grid_row: 'span 2',
+      activation: 1,
+      ids_files: ['90843886', '70843886'],
+      ids_filters: [],
+      grid_column: '1',
+      grid_row: '1',
     },
     {
       id: 4,
@@ -74,12 +77,13 @@ const dataJSObjetcFilesImegesGalleryMosaicListing = {
       number_small2: 0, 
       number_small3: 1, 
       info_small1: 'http://www.google.com',
-      file_thumbnail: 'https://picsum.photos/600/600/?image=515',
+      file_thumbnail: 'https://picsum.photos/100/100/?image=515',
       file: 'https://picsum.photos/1920/1080/?image=515',
-      activation: '',
-      ids_link: [],
-      grid_column: 'span 2',
-      grid_row: 'span 2',
+      activation: 1,
+      ids_files: [],
+      ids_filters: [],
+      grid_column: '2',
+      grid_row: '2',
     },
     {
       id: 5,
@@ -92,14 +96,15 @@ const dataJSObjetcFilesImegesGalleryMosaicListing = {
       description: 'Teste com vinculação',
       number_small1: 1, 
       number_small2: 0, 
-      number_small3: 1, 
+      number_small3: 5, 
       info_small1: 'http://www.google.com',
-      file_thumbnail: 'https://picsum.photos/600/600/?image=516',
+      file_thumbnail: 'https://picsum.photos/50/50/?image=516',
       file: 'https://picsum.photos/1920/1080/?image=516',
-      activation: '',
-      ids_link: [],
-      grid_column: 'span 2',
-      grid_row: 'span 3',
+      activation: 1,
+      ids_files: [],
+      ids_filters: [],
+      grid_column: '2',
+      grid_row: '3',
     },
     {
       id: 6,
@@ -110,16 +115,17 @@ const dataJSObjetcFilesImegesGalleryMosaicListing = {
       title: 'Título do produto de exemplo 06',
       caption: '65843886',
       description: 'Teste com vinculação',
-      number_small1: 1, 
+      number_small1: 3, 
       number_small2: 0, 
       number_small3: 1, 
       info_small1: 'http://www.google.com',
-      file_thumbnail: 'https://picsum.photos/600/600/?image=517',
+      file_thumbnail: 'https://picsum.photos/100/100/?image=517',
       file: 'https://picsum.photos/1920/1080/?image=517',
-      activation: '',
-      ids_link: [],
-      grid_column: 'span 2',
-      grid_row: 'span 2',
+      activation: 1,
+      ids_files: [],
+      ids_filters: [],
+      grid_column: '2',
+      grid_row: '2',
     },
     {
       id: 7,
@@ -134,12 +140,13 @@ const dataJSObjetcFilesImegesGalleryMosaicListing = {
       number_small2: 0, 
       number_small3: 1, 
       info_small1: 'http://www.google.com',
-      file_thumbnail: 'https://picsum.photos/600/600/?image=518',
+      file_thumbnail: 'https://picsum.photos/125/125/?image=518',
       file: 'https://picsum.photos/1920/1080/?image=518',
-      activation: '',
-      ids_link: [],
-      grid_column: 'span 2',
-      grid_row: 'span 2',
+      activation: 1,
+      ids_files: [],
+      ids_filters: [],
+      grid_column: '2',
+      grid_row: '2',
     },
   ]
 };
@@ -169,29 +176,158 @@ const dataJSObjetcFilesImegesGalleryMosaicListing = {
 */
 
 const elementHTMLFilesGalleryMosaic = document.getElementById('divFilesImagesGalleryMosaic');
-let filesGalleryMosaicPopupHTML = '';
 
 // Logic.
 for (let i = 0; i < dataJSObjetcFilesImegesGalleryMosaicListing.files.length; i++) {
   
   // Append HTML.
   elementHTMLFilesGalleryMosaic.innerHTML += `
-    <div style="grid-column: ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].grid_column}; grid-row: ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].grid_row};">
-        <img src="${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].file_thumbnail}" />
+    <div 
+      class="
+      ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small1 === 2 ?
+        ` ss-frontend-files-images-gallery-mosaic-feature`
+        :
+        ``
+      }
+      "
+      style="
+        grid-column: span ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].grid_column}; 
+        grid-row: span ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].grid_row};
+        ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small1 === 3 ?
+          `
+          background-image: url(${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].file});
+          background-repeat: no-repeat;
+          background-size: cover;
+          `
+          :
+          ``
+        }
+      ">
+        ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small1 !== 3 ?
+          `
+            <div class="
+            ss-frontend-files-images-gallery-mosaic-thumbnail-text
+            ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small3 === 1 ?
+              ` ss-frontend-files-images-gallery-mosaic-thumbnail-text1`
+              :
+              ``
+            }
+            ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small3 === 2 ?
+              ` ss-frontend-files-images-gallery-mosaic-thumbnail-text2`
+              :
+              ``
+            }
+            ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small3 === 3 ?
+              ` ss-frontend-files-images-gallery-mosaic-thumbnail-text3`
+              :
+              ``
+            }
+            ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small3 === 4 ?
+              ` ss-frontend-files-images-gallery-mosaic-thumbnail-text4`
+              :
+              ``
+            }
+            ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small3 === 5 ?
+              ` ss-frontend-files-images-gallery-mosaic-thumbnail-text5`
+              :
+              ``
+            }
+            ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small3 === 6 ?
+              ` ss-frontend-files-images-gallery-mosaic-thumbnail-text6`
+              :
+              ``
+            }
+            ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small3 === 7 ?
+              ` ss-frontend-files-images-gallery-mosaic-thumbnail-text7`
+              :
+              ``
+            }
+            ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small3 === 8 ?
+              ` ss-frontend-files-images-gallery-mosaic-thumbnail-text8`
+              :
+              ``
+            }
+          " style="
+          ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small1 === 2 ?
+            `color: #ffffff !important; `
+            :
+            ``
+          }
+          ">
+                <h3>
+                    ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].title}
+                </h3>
+                <div>
+                    Código: ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].caption}
+                </div>
+                <p>
+                    ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].description}
+                </p> 
+            </div>
+            
+            <img 
+              src="${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].file_thumbnail}"
+              alt="${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].title}"
+              class="
+              ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small2 === 1 ?
+                ` ss-frontend-files-images-gallery-mosaic-thumbnail-image1`
+                :
+                ``
+              }
+              ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small2 === 2 ?
+                ` ss-frontend-files-images-gallery-mosaic-thumbnail-image2`
+                :
+                ``
+              }
+              ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small2 === 3 ?
+                ` ss-frontend-files-images-gallery-mosaic-thumbnail-image3`
+                :
+                ``
+              }
+              ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small2 === 4 ?
+                ` ss-frontend-files-images-gallery-mosaic-thumbnail-image4`
+                :
+                ``
+              }
+              ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small2 === 5 ?
+                ` ss-frontend-files-images-gallery-mosaic-thumbnail-image5`
+                :
+                ``
+              }
+              ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small2 === 6 ?
+                ` ss-frontend-files-images-gallery-mosaic-thumbnail-image6`
+                :
+                ``
+              }
+              ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small2 === 7 ?
+                ` ss-frontend-files-images-gallery-mosaic-thumbnail-image7`
+                :
+                ``
+              }
+              ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].number_small2 === 8 ?
+                ` ss-frontend-files-images-gallery-mosaic-thumbnail-image8`
+                :
+                ``
+              }
+              " 
+            />
+          `:
+          ``
+        }
         <a href="#ssFrontendFilesImagesGalleryMosaicLightbox01-${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].id}">
           ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].id}
         </a>
     </div>
-        
-    
   `;
   
   // Insert HTML after element.
-  /**/
   elementHTMLFilesGalleryMosaic.insertAdjacentHTML('afterend', `
     <div id="ssFrontendFilesImagesGalleryMosaicLightbox01-${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].id}">
         <div class="ss-frontend-files-images-gallery-mosaic01-popup">
-            <img src="${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].file}" />
+            <img 
+              src="${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].file}" 
+              alt="${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].title}" 
+            />
             <div class="title">
                 ${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].title}
                 <a href="${dataJSObjetcFilesImegesGalleryMosaicListing.files[i].info_small1}" target="_blank" style="z-index: 9999;">
---
+++ app_js/data-filters-generic-listing.js
@@ -0,0 +1,12 @@
+const dataJSObjetcFilesImegesGalleryMosaicListing = {
+  filters_generic: [
+    {
+      id: 1,
+      sort_order: 0,
+      filter_index: 101, // 1 - image | 2 - video
+      title: 'Filter 01',
+      activation: 1,
+    },
+  ]
+  
+};
